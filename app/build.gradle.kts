0735858cbaa5c761a1603eef8dbf449bf33b9a0b
Sekiguchi <wood29st@gmail.com>
2026-07-19 12:50:35 +0900

    v1.1 icon, train interior art, ep03-ep04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sekiguchi.soundnovel"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
