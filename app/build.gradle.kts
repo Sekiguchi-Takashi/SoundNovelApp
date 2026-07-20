c501af88c22abe3887886d74865089583c324ddd
Sekiguchi-Takashi <Sekiguchi-Takashi@users.noreply.github.com>
2026-07-20 14:18:11 +0900

    v1.2 title and cottage icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sekiguchi.soundnovel"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     signingConfigs {
