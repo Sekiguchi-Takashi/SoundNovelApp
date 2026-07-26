9c450178af158063e315b86edc8f7d52c6bc741a
Sekiguchi-Takashi <Sekiguchi-Takashi@users.noreply.github.com>
2026-07-26 10:05:49 +0900

    v4.6 unify three inconsistencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sekiguchi.soundnovel"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "4.5"
+        versionCode = 12
+        versionName = "4.6"
     }
 
     signingConfigs {
